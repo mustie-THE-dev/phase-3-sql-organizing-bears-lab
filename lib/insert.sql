8ec71537906758fbbf2353add36c11966201045d
Simon kamau <simon.kamau@student.moringaschool.com>
2022-08-23 17:06:20 +0300

    commit tree dd6995c78a542073a66305446b2eed0b51fc8cc6

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'F', 'blue', 'optimistic', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'dark', 'optimstic', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'dark', 'loving', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'dark', 'laidback', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'gray', 'goofy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'dark', 'pessimistic', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'brown', 'choleric', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'M', 'gray', 'phelgmatiic', TRUE);
